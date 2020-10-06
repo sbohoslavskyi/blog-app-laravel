be127af095cf781e9f50e0f46b2200bf2a329d83
Serhii Bohoslavskyi <sbohoslavskyi@gmail.com>
2020-10-06 18:14:16 +0300

    Markup base view for primary post

--- resources/views/web/sections/page/index.blade.php
+++ resources/views/web/sections/page/index.blade.php
@@ -3,6 +3,18 @@
 @section('title', 'Home')
 
 @section('content')
+    <div class="post-cover" style="background-image: url(http://lorempixel.com/1920/1080/);">
+        <div class="container h-100">
+            <div class="post-cover-content">
+                <time>{{ $primary_post->published_at }}</time>
+                <a href="{{ route('post.show', ['post' => $primary_post->slug]) }}">
+                    <h1>{{ $primary_post->title }}</h1>
+                </a>
+                <a href="{{ route('post.show', ['post' => $primary_post->slug]) }}">Read more</a>
+            </div>
+        </div>
+    </div>
+
     <div class="container">
         <div class="row">
             <div class="col-12">
