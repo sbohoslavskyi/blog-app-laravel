cb182712e747592369ca59da776be9c0cb8deba0
Serhii Bohoslavskyi <sbohoslavskyi@gmail.com>
2020-10-06 13:48:52 +0300

    Add base markup for post page

--- resources/views/web/layout.blade.php
+++ resources/views/web/layout.blade.php
@@ -5,14 +5,16 @@
         <meta name="viewport"
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <title>@yield('title') | Blog</title>
     </head>
     <body>
-        <header>
+        {{--<header>
             @include('web.layout.navigation')
-        </header>
+        </header>--}}
         <main>
             @yield('content')
         </main>
+        <script src="{{ asset('js/app.js') }}"></script>
     </body>
 </html>
--- resources/views/web/sections/post/show.blade.php
+++ resources/views/web/sections/post/show.blade.php
@@ -3,5 +3,27 @@
 @section('title', $post->title)
 
 @section('content')
-    <p>{{ $post->body }}</p>
+    <div class="post" style="background: url(https://lorempixel.com/1920/1080) no-repeat center center fixed;">
+        <header class="container">
+            <h1>{{ $post->title }}</h1>
+        </header>
+        <p class="container body">
+            Laborum qui voluptates non labore est quidem. Et non ut quo aut. Voluptatem ratione optio esse deleniti ut.
+            Laborum qui voluptates non labore est quidem. Et non ut quo aut. Voluptatem ratione optio esse deleniti ut.
+            Laborum qui voluptates non labore est quidem. Et non ut quo aut. Voluptatem ratione optio esse deleniti ut.
+            Laborum qui voluptates non labore est quidem. Et non ut quo aut. Voluptatem ratione optio esse deleniti ut.
+            Laborum qui voluptates non labore est quidem. Et non ut quo aut. Voluptatem ratione optio esse deleniti ut.
+            Laborum qui voluptates non labore est quidem. Et non ut quo aut. Voluptatem ratione optio esse deleniti ut.
+            Laborum qui voluptates non labore est quidem. Et non ut quo aut. Voluptatem ratione optio esse deleniti ut.
+            Laborum qui voluptates non labore est quidem. Et non ut quo aut. Voluptatem ratione optio esse deleniti ut.
+            Laborum qui voluptates non labore est quidem. Et non ut quo aut. Voluptatem ratione optio esse deleniti ut.
+            Laborum qui voluptates non labore est quidem. Et non ut quo aut. Voluptatem ratione optio esse deleniti ut.
+            Laborum qui voluptates non labore est quidem. Et non ut quo aut. Voluptatem ratione optio esse deleniti ut.
+            Laborum qui voluptates non labore est quidem. Et non ut quo aut. Voluptatem ratione optio esse deleniti ut.
+            Laborum qui voluptates non labore est quidem. Et non ut quo aut. Voluptatem ratione optio esse deleniti ut.
+            Laborum qui voluptates non labore est quidem. Et non ut quo aut. Voluptatem ratione optio esse deleniti ut.
+            Laborum qui voluptates non labore est quidem. Et non ut quo aut. Voluptatem ratione optio esse deleniti ut.
+            Laborum qui voluptates non labore est quidem. Et non ut quo aut. Voluptatem ratione optio esse deleniti ut.
+        </p>
+    </div>
 @endsection
