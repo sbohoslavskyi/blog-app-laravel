b9b1a0c2b44975fe8dae6caa3ccf9152a0a9b948
Serhii Bohoslavskyi <sbohoslavskyi@gmail.com>
2020-10-07 10:41:09 +0300

    Update primary post view

--- resources/views/web/sections/page/index.blade.php
+++ resources/views/web/sections/page/index.blade.php
@@ -3,9 +3,9 @@
 @section('title', 'Home')
 
 @section('content')
-    <div class="post-cover" style="background-image: url(http://lorempixel.com/1920/1080/);">
+    <div class="post-cover" style="background-image: url({{ $primary_post->image }});">
         <div class="container h-100">
-            <div class="post-cover-content">
+            <div class="post-cover-content" data-aos="zoom-in-up">
                 <time>{{ $primary_post->published_at }}</time>
                 <a href="{{ route('post.show', ['post' => $primary_post->slug]) }}">
                     <h1>{{ $primary_post->title }}</h1>
